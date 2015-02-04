93a032b849f45b9cc64c3fa21399385172c3853d
louishol <louis_hol@hotmail.com>
2015-02-04 10:14:25 +0100

    dasdsadsad

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('test123!');
+  response.send('Changelalala!');
 });
 
 app.listen(app.get('port'), function() {
